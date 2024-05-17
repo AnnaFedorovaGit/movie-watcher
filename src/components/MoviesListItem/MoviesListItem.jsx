981e736ca1445e91e74cfa26be80b2e61705192f
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-05-17 21:34:09 +0300

    fix tooltip

--- src/components/MoviesListItem/MoviesListItem.jsx
+++ src/components/MoviesListItem/MoviesListItem.jsx
@@ -1,31 +1,37 @@
+import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import Zoom from '@mui/material/Zoom'
-import { styled } from '@mui/material/styles'
-import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip'
 
-import css from 'components/MoviesListItem/MoviesListItem.module.css'
+import { Typography } from '@mui/material'
+import Zoom from '@mui/material/Zoom'
+import HtmlTooltip from '@mui/material/Tooltip'
+
 import defaultImage from 'images/default-image.jpg'
 import { ReactComponent as IconInfo } from 'images/info_icon.svg'
+import css from 'components/MoviesListItem/MoviesListItem.module.css'
 
 
 const MoviesListItem = ({ movie, page }) => {
     const location = useLocation();
     const { id, title, poster_path, overview, name, vote_average, release_date } = movie;
     const imagePath = `https://image.tmdb.org/t/p/w500${poster_path}`;
- 
-    const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
-        <Tooltip {...props} classes={{ popper: className }} />
-        ))({
-            [`& .${tooltipClasses.tooltip}`]: {
-                backgroundColor: '#ffffff',
-                color: 'var(--primary-dark-color)',
-                lineHeight: '137%',
-                fontFamily: 'DM Sans, sans-serif',
-                fontSize: '15px',
-                border: '1px solid var(--primary-dark-color)',
-                boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
-            },
-    });
+        
+    const CustomTooltip = () => {
+        return (
+            <HtmlTooltip TransitionProps={{ timeout: 500 }} TransitionComponent={Zoom} arrow title={
+                <React.Fragment  >
+                    <Typography color="inherit">{title}</Typography>
+                    <br/>
+                    <b>{'Overview: '}</b>
+                    <p>{`  ''${overview}''`}</p>
+                </React.Fragment>
+                }
+            >
+            <div className={css.infoWrapper}>
+                <IconInfo width='26px' height='26px'/>
+            </div>
+            </HtmlTooltip>
+        )
+    }
     
     return (
         <li className={css.galleryItem} key={id} >
@@ -38,15 +44,7 @@ const MoviesListItem = ({ movie, page }) => {
                         (<p className={css.ratingText}>{vote_average && Math.round(10 * vote_average)}<span className={css.ratingPersent}>%</span></p>) :
                         (<p className={css.ratingText}>N/A</p>)}                    
                 </div>
-                {overview !== 0 &&
-                    (<>
-                    <CustomTooltip TransitionComponent={Zoom} title={overview} placement="top-start">
-                        <div className={css.infoWrapper}>
-                            <IconInfo width='26px' height='26px'/>
-                        </div>
-                        </CustomTooltip>
-                    </>)
-                }
+                {overview !== 0 && <CustomTooltip />}
                 <p className={css.text}>{title} {release_date && `(${(release_date).slice(0, 4)})`}</p>
             </Link>
         </li>
